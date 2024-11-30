773ece25fcda61045cfcdaac52871cba5b6b4415
icchy-san <icchy.tech@gmail.com>
2024-11-30 20:08:39 +0900

    implemented db initializer and auto migration code.

---
+++ db/db.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"fmt"
+	"github.com/icchy-san/uctest/config"
+	"github.com/icchy-san/uctest/db/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// define db interface
+type DB interface {
+}
+
+type db struct {
+	client *gorm.DB
+}
+
+// initDB returns db client by connection
+func initDB(env *config.Env) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		env.DBHost,
+		env.DBUser,
+		env.DBPassword,
+		env.DBName,
+	)
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	if migrateErr := database.AutoMigrate(
+		model.Invoice{},
+		model.Company{},
+		model.BusinessPartner{},
+		model.Bank{},
+		model.User{},
+	); migrateErr != nil {
+		return nil, migrateErr
+	}
+
+	return database, nil
+}
+
+// return initialized db
+func New(env *config.Env) (DB, error) {
+	client, err := initDB(env)
+	if err != nil {
+		return nil, err
+	}
+
+	return &db{
+		client: client,
+	}, nil
+}
